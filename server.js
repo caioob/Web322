92506933090acaf97d1cbb7feaea99b4fdfb4389
caioob <caio.basaglia@outlook.com>
2019-02-09 16:04:02 -0500

    a3 started

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ var express = require("express");
 var path = require("path");
 var app = express();
 var data = require("./data-service.js");
+var multer = require("multer");
 
 var port = process.env.PORT || 8080;
 
@@ -23,8 +24,19 @@ function onHttpStart(){
     console.log("Express http server listening on port " +port);
 }
 
+//multer setup
+const storage = multer.diskStorage({
+    destination: "./public/images/uploaded",
+    filename: function (req, file, cb){
+        cb(null, Date.now()+path.extname(file.originalname));
+    }
+});
+
+//creating upload variable
+const upload = multer({ storage: storage });
+
 //setting up css
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //setting up home 
 app.get("/", function (req, res){
@@ -43,33 +55,38 @@ app.get("/employees", function (req, res){
     .catch((err) => {res.json(err)})
 });
 
+//emplyees/add route
+app.get("/employees/add", function(req,res){
+    res.sendFile(path.join(__dirname, "/views/addEmployee.html"));
+});
+
+//Images/add route
+app.get("/images/add", function(req,res){
+    res.sendFile(path.join(__dirname, "/views/addImage.html"));
+});
+
+//Images post route
+app.post("/images/add", upload.single("photo"), function(req, res){
+    res.redirect("/images");
+});
+
+//managers route
 app.get("/managers", function (req, res){
     data.getManagers()
     .then((data) => {res.json(data)})
     .catch((err) => {res.json(err)})
 });
-
+//departments route
 app.get("/departments", function (req, res){
     data.getDepartments()
     .then((data) => {res.json(data)})
     .catch((err) => {res.json(err)})
 });
 
+
 //Error Satuts Page (Error 404)
 app.use(function(req, res){
     res.status(404).send("<h1 style='color: red;'>Error 404. Page Not Found</h1>");
-});
-
-app.get("/managers", function(req, res){
-
-});
-
-app.get("/departments", function(req, res){
-
-});
-
-app.use( function(req, res){
-    res.status(404).send("<h1 style='color: red;'> Error 404. Page not found.</h1>");
 });
 
 data.initialize()
