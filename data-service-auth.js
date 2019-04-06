f43b4bba0ee06d341f22ea31268eec816009b35c
caioob <caio.basaglia@outlook.com>
2019-04-05 20:49:52 -0400

    Actually done

--- data-service-auth.js
+++ data-service-auth.js
@@ -89,33 +89,3 @@ module.exports.checkUser = (userData) => {
             }) 
     });
 }
-
-module.exports.checkUser = function(userData) { 
-    return new Promise((resolve, reject) => { 
-        User.find({userName: userData.userName}) .exec()
-            .then((users) => { 
-                if(users.length == 0){ 
-                    reject('Unable to find user: '+ userData.userName); 
-                }else{ 
-                    bcrypt.compare(userData.password, users[0].password).then((res)=>{ 
-                        if(res===true){ 
-                            users[0].loginHistory.push({dateTime: (new Date()).toString(), userAgent: userData.userAgent}); 
-                            User.update( 
-                                { userName: users[0].userName }, 
-                                { $set: {loginHistory: users[0].loginHistory }}, 
-                                { multi: false } 
-                            ).exec().then((() => { 
-                                resolve(users[0]); 
-                            })).catch((err) => { 
-                                reject("There was an error verifying the user: " + err); 
-                            }); 
-                        } else{ 
-                            reject('Incorrect Password for user: '+ userData.userName); 
-                        } 
-                    }) 
-                } 
-            }).catch(() => { 
-                reject('Unable to find user: '+userData.userName); 
-            }) 
-    }); 
-} 
